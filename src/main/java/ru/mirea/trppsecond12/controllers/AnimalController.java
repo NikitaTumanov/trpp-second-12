b9dbf7856feaf1d2d4ce01cd9c0e34d8c927a2f6
Nikita <tuman0v.nik@yandex.ru>
2021-03-18 13:38:09 +0300

    +changes

--- src/main/java/ru/mirea/trppsecond12/controllers/AnimalController.java
+++ src/main/java/ru/mirea/trppsecond12/controllers/AnimalController.java
@@ -1,9 +1,10 @@
-package ru.mirea.trpp_second_12.controllers;
+package ru.mirea.trppsecond12.controllers;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import ru.mirea.trppsecond12.entity.Animal;//Недостающий импорт
 
 import java.io.InputStreamReader;
 import java.util.List;
@@ -18,7 +19,8 @@ public class AnimalController {
 
     /** Конструктор. */
     public AnimalController() {
-        animalList = new CsvToBeanBuilder<Animal>(new InputStreamReader(this.getClass().getResourceAsStream("/MOCK_DATA.csv"))).withType(Animal.class).build().parse();
+        animalList = new CsvToBeanBuilder<Animal>(new InputStreamReader(this.getClass()
+                .getResourceAsStream("/MOCK_DATA.csv"))).withType(Animal.class).build().parse();
     }
 
     /**
--- src/main/java/ru/mirea/trppsecond12/entity/HealthResponse.java
+++ src/main/java/ru/mirea/trppsecond12/entity/HealthResponse.java
@@ -1,4 +1,4 @@
-package ru.mirea.trpp_second_12.entity;
+package ru.mirea.trppsecond12.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
